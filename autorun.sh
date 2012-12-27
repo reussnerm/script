5e9a52195317dd38c9bf17968559747764984d73
Matthieu Reussner <matthieu@reussner.ch>
2012-12-27 13:16:24 +0100

    my .bin folder

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-remind  -z -k'/home/reussnerm/.bin/rem_notify.sh %s | ~/.bin/dbus_wrapper_no_x.sh &' /home/steelido/.reminders &
+remind  -z -k'/home/reussnerm/.bin/rem_notify.sh %s | ~/.bin/dbus_wrapper_no_x.sh &' /home/reussnerm/.reminders &
 setxkbmap -option 'grp:alt_space_toggle,ctrl:nocaps' 'ch(fr),us'
 setxkbmap -model thinkpad
 autocutsel -fork
